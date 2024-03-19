b673448d6cced40bb7a893ca21aac77b4d0018e8
gegagavasheli <gegagavasheli@gmail.com>
2024-03-19 17:39:53 +0400

    trying to raise conflict

--- homework/src/test/java/CookiesTest.java
+++ homework/src/test/java/CookiesTest.java
@@ -75,7 +75,9 @@ public class CookiesTest {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[@class="+Constants.XPATHAUTO)));
         element.sendKeys(Keys.ARROW_DOWN);
         element.sendKeys(Keys.ENTER);
+        System.out.println("This line should raise a conflict");
     }
+
 
     @AfterClass
     public void turnDown() {
